3887612f60c998e5d8adf278a01e0afeffdbf2ce
marksmerritt <markmerritt804@gmail.com>
2018-02-18 13:22:31 -0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 
+# Create Users
 20.times do 
   user = User.new(
     name: Faker::GameOfThrones.character,
@@ -12,7 +13,7 @@ end
 
 users = User.all
 
-
+# Create Wikis
 100.times do 
   Wiki.create!(
     title: Faker::FamilyGuy.quote,
@@ -21,6 +22,35 @@ users = User.all
     user: users.sample
   )
 end
+
+# Create Users with Different Roles
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+admin.skip_confirmation!
+admin.save!
+
+standard = User.new(
+  name: 'Standard User',
+  email: 'standard@example.com',
+  password: 'helloworld',
+  role: 'standard'
+)
+standard.skip_confirmation!
+standard.save!
+
+premium = User.new(
+  name: 'Premium User',
+  email: 'premium@example.com',
+  password: 'helloworld',
+  role: 'premium'
+)
+premium.skip_confirmation!
+premium.save!
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
